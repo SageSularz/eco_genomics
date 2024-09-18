7e62c2284f224391852e8424aa5b2460eff0b4ac
Sage Sularz <ssularz@node224.cluster>
2024-09-18 16:10:58 -0400

    09-17-24 class work// Filtering

--- population_genomics/docs/01_VCF_filtering.r
+++ population_genomics/docs/01_VCF_filtering.r
@@ -3,10 +3,11 @@ library(vcfR)
 setwd("/gpfs1/cl/pbio3990/PopulationGenomics/")
 
 list.files()
+list.files("variants/")
 
 vcf <- read.vcfR("variants/Centaurea_filtered.vcf.gz")
-
-head(vcf)
+vcf #gives quick sum sample #, chromo #, size, etc 
+head(vcf) # peak into the organization of the file...meta, fixed,
 
 dna <- ape::read.dna("reference/GCA_030169165.1_ASM3016916v1_genomic.fa.gz", format="fasta")
 
@@ -19,3 +20,56 @@ plot(chr1)
 pdf(file="~/projects/eco_genomics/population_genomics/figures/ChromoPlot.pdf")
 chromoqc(chr1, xlim=c(1e1, 1.1e8))
 dev.off()
+
+### 09-17-24
+DP <- extract.gt(vcf, element = "DP", as.numeric = T)
+dim(DP)
+DP[1:5,1:10] #how many rows and individuals 
+
+quantile(DP, na.rm = T) #what does the dist. look like?
+
+DP[DP==0] <- NA
+
+#Visualize the matrix of DP and missingness in 
+
+heatmap.bp(DP[1:1000,], rlabels = F, clabels = F)
+
+library(SNPfiltR) #visualize filtering step and apply filters then export vcf file so you dont have to repeat things
+
+vcf.filt <- hard_filter(vcf, depth = 3) #what val do we want to assgin to hard filter?
+
+                                        #could explore more values DP 5, 10....
+vcf.filt <- max_depth(vcf.filt, maxdepth = 60) #filter out genos with >60 reads/SNPs
+
+meta <- read.csv("metadata/meta4vcf.csv", header=T)
+
+meta2 <- meta[,c(1,4)]  #pull ID and Region
+head(meta2)
+
+names(meta2) <- c("id", "pop")
+
+meta2$id = as.factor(meta2$id)
+meta2$pop = as.factor(meta2$pop)
+
+vcf.filt.indMiss <- missing_by_sample(vcf.filt,
+                                      popmap=meta2,
+                                      cutoff=0.75) #0 wouldnt filter anyone 0.5 would filter half
+vcf.filt.indMiss <- filter_biallelic(vcf.filt.indMiss)
+vcf.filt.indMiss <- min_mac(vcf.filt.indMiss, min.mac = 1) #how many times do you need to see a allele to filter it?
+
+vcf.filt.indSNPMiss <- missing_by_snp(vcf.filt.indMiss, cutoff = 0.5) #50% is standard for completness its a good balance 
+
+DP2 <- extract.gt(vcf.filt.indSNPMiss,
+                  element = "DP",
+                  as.numeric = T)
+
+heatmap.bp(DP2[1:5000, ],
+           rlabels = F, clabels = F)
+
+write.vcf(vcf.filt.indSNPMiss, 
+          "~/projects/eco_genomics/population_genomics/outputs/vcf_final.filtered.vcf.gz")
+
+
+
+
+
--- population_genomics/popgen_notes.md
+++ population_genomics/popgen_notes.md
@@ -7,3 +7,19 @@
 #this was a virtual day - light notes #dear god dont forget the head command again #sffr if you take one thing away from today let it be that \^\^
 
 ### 09-12-24 Viewing VCF Files and talking about filtering
+
+### 09-17-24 Finishing up with filtering and VCFs then getting into diversity stats
+
+0/0=ref hom, 0/1=het, 1/1= alt hom, ./.= NA
+
+#grouping-- df[rows,columns] df[ ,c(1,4)]
+
+Filtering Stratigies== /\_\\ find middle zone with pros and trade offs of each
+
+#depth: low dp (limited accuracy of GF call)/// use hard_filter funct High dp (assembly error, paralogy-reads from duplicated genes mapping to same position) use max_depth func #missingness - indevidual level and SNP level
+
+#low frequency alleles
+
+#Questions? #\>\> how does file types effect head or similar commands?
+
+-   start to visualize values associatied with each "triangle point"
